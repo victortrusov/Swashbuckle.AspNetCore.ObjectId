6893c3daa0e682c4a5e79e6b74ab7d7823ff7a21
Victor Trusov <mercurochromo@gmail.com>
2020-07-09 03:59:25 +0300

    Add files via upload

---
+++ AuthenticationRequirementsOperationFilter.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Middleware.Swagger
+{
+    public class AuthenticationRequirementsOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var actionMetadata = context.ApiDescription.ActionDescriptor.EndpointMetadata;
+            var isAuthorized = actionMetadata.Any(metadataItem => metadataItem is AuthorizeAttribute);
+            var allowAnonymous = actionMetadata.Any(metadataItem => metadataItem is AllowAnonymousAttribute);
+
+            if (!isAuthorized || allowAnonymous)
+            {
+                return;
+            }
+            if (operation.Parameters == null)
+                operation.Parameters = new List<OpenApiParameter>();
+
+            operation.Security = new List<OpenApiSecurityRequirement>();
+
+            //Add JWT bearer type
+            operation.Security.Add(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                // Definition name. 
+                                // Should exactly match the one given in the service configuration
+                                Id = "Bearer"
+                            }
+                        }, new string[0]
+                    }
+                }
+            );
+
+            operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
+            operation.Responses.Add("403", new OpenApiResponse { Description = "Forbidden" });
+        }
+    }
+}
