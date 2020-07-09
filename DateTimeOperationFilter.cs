6cf04dd91ef85bc501ba0c34238b2e31f458c34c
Victor Trusov <mercurochromo@gmail.com>
2020-07-09 13:37:47 +0300

    Changed date filters

--- DateTimeOperationFilter.cs
+++ DateTimeOperationFilter.cs
@@ -8,6 +8,8 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 using Domain.Extensions;
 using System.ComponentModel.DataAnnotations;
 using System.Reflection;
+using System.Text.Json.Serialization;
+using Domain.JsonConverters;
 
 namespace Middleware.Swagger
 {
@@ -20,16 +22,22 @@ namespace Middleware.Swagger
                 {
                     if (p.Schema.Format == "date-time")
                     {
+                        p.Example = new OpenApiString(DateTime.Now.ToIsoDateTimeString());
+
+                        // check if datetime for sure
                         var name = char.ToUpperInvariant(p.Name[0]) + p.Name.Substring(1);
-                        var classProp = context.MethodInfo.GetParameters().FirstOrDefault()?.ParameterType?
-                            .GetProperties().FirstOrDefault(x => x.Name == name);
-                        var typeAttr = classProp != null
-                        ? (DataTypeAttribute)classProp.GetCustomAttribute<DataTypeAttribute>()
-                        : null;
-                        if (typeAttr != null && typeAttr.DataType == System.ComponentModel.DataAnnotations.DataType.DateTime)
-                            p.Example = new OpenApiString(DateTime.Now.ToIsoDateTimeString());
-                        else
-                            p.Schema.Format = "date";
+                        var classProp = context.MethodInfo.GetParameters().FirstOrDefault()?.ParameterType?.GetProperties().FirstOrDefault(x => x.Name == name);
+
+                        if (classProp != null)
+                        {
+                            var typeAttr = (DataTypeAttribute)classProp.GetCustomAttribute<DataTypeAttribute>();
+                            var converterAttr = (JsonConverterAttribute)classProp.GetCustomAttribute<JsonConverterAttribute>();
+                            if (
+                                (typeAttr == null || typeAttr.DataType != System.ComponentModel.DataAnnotations.DataType.DateTime) &&
+                                (converterAttr == null || converterAttr.ConverterType != typeof(DateWithTimeConverter))
+                            )
+                                p.Schema.Format = "date";
+                        }
                     }
 
                     if (p.Schema.Format == "date")
--- DateTimeSchemaFilter.cs
+++ DateTimeSchemaFilter.cs
@@ -8,6 +8,8 @@ using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 using MongoDB.Bson;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using Newtonsoft.Json;
+using Domain.JsonConverters;
 
 namespace Middleware.Swagger
 {
@@ -19,18 +21,25 @@ namespace Middleware.Swagger
             {
                 if (p.Value.Format == "date-time")
                 {
-                    var name = char.ToUpperInvariant(p.Key[0]) + p.Key.Substring(1);
+                    p.Value.Example = new OpenApiString(DateTime.Now.ToIsoDateTimeString());
+
                     // check if datetime for sure
+                    var name = char.ToUpperInvariant(p.Key[0]) + p.Key.Substring(1);
                     var classProp = context.Type.GetProperties()
                         .FirstOrDefault(x => x.Name == name);
-                    var typeAttr = classProp != null
-                        ? (DataTypeAttribute)classProp.GetCustomAttribute<DataTypeAttribute>()
-                        : null;
-                    if (typeAttr != null && typeAttr.DataType == System.ComponentModel.DataAnnotations.DataType.DateTime)
-                        p.Value.Example = new OpenApiString(DateTime.Now.ToIsoDateTimeString());
-                    else
-                        p.Value.Format = "date";
+
+                    if (classProp != null)
+                    {
+                        var typeAttr = (DataTypeAttribute)classProp.GetCustomAttribute<DataTypeAttribute>();
+                        var converterAttr = (JsonConverterAttribute)classProp.GetCustomAttribute<JsonConverterAttribute>();
+                        if (
+                            (typeAttr == null || typeAttr.DataType != System.ComponentModel.DataAnnotations.DataType.DateTime) &&
+                            (converterAttr == null || converterAttr.ConverterType != typeof(DateWithTimeConverter))
+                        )
+                            p.Value.Format = "date";
+                    }
                 }
+
                 if (p.Value.Format == "date")
                     p.Value.Example = new OpenApiString(DateTime.Now.ToIsoDateString());
             }
